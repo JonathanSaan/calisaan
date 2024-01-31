41850f09739eb5716f30caa50862f21373cd104b
jonathansaandev <jonathansaandev@gmail.com>
2024-01-30 21:50:22 -0300

    error page and meta

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex justify-center flex-col items-center h-screen w-full text-center">
+    <h1 class="text-9xl font-bold">404</h1>
+    <h2 class="text-4xl">Page not found.</h2>
+    <button class="btn my-14 py-2 px-4 rounded text-2xl bg-red-500" @click="handleClearError">Go back home</button>
+  </div>
+</template>
+
+<script setup>
+  defineProps(['error'])
+  
+  const handleClearError = () => clearError({ redirect: '/' })
+</script>
+
+<style scoped>
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
   css: ['@fortawesome/fontawesome-free/css/all.css'],
+  app: {
+    head: {
+      title: 'CaliSaan',
+      meta: [
+        { name: 'description', content: 'Discover top-quality fitness products to help you reach your health and wellness goals. From cutting-edge workout gear to performance-enhancing supplements, CaliSaan has everything you need for a fit and active lifestyle. Start your journey to a healthier you today!' }
+      ],
+    }
+  }
 })
