fcd3566e69722824fcf4f30a479b8987a9b79da4
jonathansaandev <jonathansaandev@gmail.com>
2024-02-15 19:25:50 -0300

    Update error.vue button styling

--- error.vue
+++ error.vue
@@ -2,7 +2,7 @@
   <div class="flex justify-center flex-col items-center h-screen w-full text-center">
     <h1 class="text-9xl font-bold">404</h1>
     <h2 class="text-4xl">Page not found.</h2>
-    <button class="btn max-lg:my-9 my-14 py-2 px-4 rounded text-2xl bg-black text-white" @click="handleClearError">Go back home</button>
+    <button class="btn max-lg:my-9 my-14 py-4 px-6 text-2xl bg-black hover:bg-zinc-900 focus:bg-zinc-800 text-white rounded-lg shadow-md outline-none" @click="handleClearError">Go back home</button>
   </div>
 </template>
 
