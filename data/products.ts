83e32f8a7d4cef7f72a40a334eb6e995a49309d0
jonathansaandev <jonathansaandev@gmail.com>
2024-02-15 01:46:44 -0300

    Add two new Basic Tee products

--- data/products.ts
+++ data/products.ts
@@ -70,6 +70,26 @@ const products: Product[] = [
     price: '$35',
     color: 'Black',
   },
+  {
+    id: 7,
+    name: 'Basic Tee',
+    slug: 'basic-tee',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    description: "A versatile and comfortable Basic Tee for everyday wear. This classic black tee is crafted with high-quality materials, providing a perfect fit and lasting durability. Whether you're dressing up or keeping it casual, the Basic Tee is a wardrobe essential.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 8,
+    name: 'Basic Tee',
+    slug: 'basic-tee',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    description: "A versatile and comfortable Basic Tee for everyday wear. This classic black tee is crafted with high-quality materials, providing a perfect fit and lasting durability. Whether you're dressing up or keeping it casual, the Basic Tee is a wardrobe essential.",
+    price: '$35',
+    color: 'Black',
+  },
 ];
 
 export default products;
