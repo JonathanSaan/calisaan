cd9a3edfd87e0206c8670c0330a5eb9719d5ac36
jonathansaandev <jonathansaandev@gmail.com>
2024-02-15 19:28:55 -0300

    Update nuxt.config.ts with new modules and build options

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,23 @@
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
-  css: ['@fortawesome/fontawesome-free/css/all.css'],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image"],
+  css: ["@fortawesome/fontawesome-free/css/all.css"],
+  target: "server",
+  build: {
+    extractCSS: true,
+    terser: {
+      terserOptions: {
+        compress: {
+          drop_console: true
+        }
+      }
+    }
+  },
   app: {
     head: {
-      title: 'CaliSaan',
+      title: "CaliSaan",
       meta: [
-        { name: 'description', content: 'Discover top-quality fitness products to help you reach your health and wellness goals. From cutting-edge workout gear to performance-enhancing supplements, CaliSaan has everything you need for a fit and active lifestyle. Start your journey to a healthier you today!' }
+        { name: "description", content: "Discover top-quality fitness products to help you reach your health and wellness goals. From cutting-edge workout gear to performance-enhancing supplements, CaliSaan has everything you need for a fit and active lifestyle. Start your journey to a healthier you today!" }
       ],
     }
-  }
-})
+  },
+});
