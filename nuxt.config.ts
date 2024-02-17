2c166ad36884150aac2c4c41e097ee9d9169a26a
jonathansaandev <jonathansaandev@gmail.com>
2024-02-16 21:35:52 -0300

    Update nuxt.config.ts with buildModules and image formats

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@nuxt/image"],
+  buildModules: ["@nuxt/image"],
   css: ["@fortawesome/fontawesome-free/css/all.css"],
-  target: "server",
+  target: "static",
   build: {
     extractCSS: true,
     terser: {
@@ -12,12 +13,25 @@ export default defineNuxtConfig({
       }
     }
   },
+  image: {
+    formats: {
+      webp: {
+        quality: 80
+      }
+    }
+  },
   app: {
     head: {
       title: "CaliSaan",
       meta: [
-        { name: "description", content: "Discover top-quality fitness products to help you reach your health and wellness goals. From cutting-edge workout gear to performance-enhancing supplements, CaliSaan has everything you need for a fit and active lifestyle. Start your journey to a healthier you today!" }
+        { name: "description", content: "Discover top-quality fitness products to help you reach your health and wellness goals. From cutting-edge workout gear to performance-enhancing supplements, CaliSaan has everything you need for a fit and active lifestyle. Start your journey to a healthier you today!" },
+        { name: "og:description", content: "Discover top-quality fitness products to help you reach your health and wellness goals. From cutting-edge workout gear to performance-enhancing supplements, CaliSaan has everything you need for a fit and active lifestyle. Start your journey to a healthier you today!" },
+        { name: "og:type", content: "website" },
+        { name: "og:locale", content: "en_US" },
       ],
+      link: [
+        { rel: "icon", type: "image/png", href: "/favicon.ico" }
+      ]
     }
   },
 });
