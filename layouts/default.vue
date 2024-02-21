9fe8687dad1eb25b650a1f1232694927ab1f3743
jonathansaandev <jonathansaandev@gmail.com>
2024-02-21 18:54:16 -0300

    feat: shopping cart functionality

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col min-h-screen bg-white text-black">
-    <header class="shadow-sm  flex justify-between items-center max-lg:px-6 px-8 max-lg:h-24 h-24 text-center">
+    <header class="shadow-sm flex justify-between items-center max-lg:px-6 px-8 max-lg:h-24 h-24 text-center">
       <NuxtLink to="/" class="font-bold max-lg:text-3xl text-4xl">CaliSaan</NuxtLink>
       <ul class="flex lg:mr-1 align-items items-center h-9">
         <li>
@@ -12,9 +12,25 @@
           </form>
         </li>
         <li class="max-sm:ml-1 max-md:ml-4 ml-5">
-          <button>
+          <button label="Open" @click="isOpen = true">
             <Icon name="clarity:shopping-cart-solid" color="black" size="22" title="Add to cart" aria-hidden="true" />
           </button>
+          
+          <USlideover v-model="isOpen" :ui="{ background: 'dark:bg-white', overlay: { background: 'dark:bg-black/65' } }">
+      		<UCard class="flex flex-col flex-1 text-black" :ui="{ background: 'dark:bg-white', body: { base: 'flex-1' }, ring: '', divide: 'divide-y divide-gray-700 dark:divide-gray-700' }">
+        	  <template #header>
+          		<div class="flex items-center justify-between">
+          		  <h3 class="text-2xl leading-6">Cart</h3>
+          		  
+          		  <button label="Open" class="my-1 ml-auto" @click="isOpen = false">
+            		<Icon name="i-heroicons-x-mark-20-solid" color="black" size="25" title="Add to cart" aria-hidden="true" />
+          		  </button>
+          		</div>
+        	  </template>
+        	  
+        	  <h4 class="mt-3 text-lg text-center">You cart is empty</h4>
+      		</UCard>
+    	  </USlideover>
         </li>
       </ul>
     </header>
@@ -30,5 +46,6 @@
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
+const isOpen = ref(false)
 </script>
