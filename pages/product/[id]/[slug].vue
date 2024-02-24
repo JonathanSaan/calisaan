2725fa2be1d16b8512d9f5c8f7a3a26e581bc7e7
jonathansaandev <jonathansaandev@gmail.com>
2024-02-23 23:09:56 -0300

    Refactor product detail component and optimize related products carousel

--- pages/product/[id]/[slug].vue
+++ pages/product/[id]/[slug].vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex lg:justify-center">
-    <div v-if="product && !pending" class="flex flex-col">
+    <div v-if="!pending && product" class="flex flex-col">
       <div class="flex mx-auto mt-5 lg:mt-8 max-lg:flex-col max-lg:px-4 w-screen lg:w-[70rem] xl:w-[86rem] lg:h-[54rem]">
         <div class="flex flex-col xl:flex-row-reverse">
   		  <NuxtImg
@@ -8,7 +8,7 @@
     		:alt="product.imageAlt"
     		class="max-lg:mx-auto max-sm:h-[38rem] max-md:h-[48rem] max-lg:h-[52rem] lg:min-h-[54rem] w-[40rem] pointer-events-none"
   		  />
-  		  <div class="flex xl:flex-col max-xl:mt-5 lg:mr-9 justify-between">
+  		  <div class="flex xl:flex-col max-xl:mt-5 lg:mr-8 justify-between">
     		<NuxtImg
       		  v-for="(image, index) in product.imageSrc"
       		  :key="index"
@@ -34,24 +34,24 @@
       
       <div class="mt-20 lg:mt-56 xl:mt-20 pb-10 lg:pb-16">
         <h2 class="max-lg:px-4 mb-9 lg:mb-8 text-3xl font-bold">Related Products</h2>
-        <ul class="flex max-lg:px-4 w-full gap-4 overflow-x-auto pt-1 w-screen lg:w-[70rem] xl:w-[86rem]">
-          <li v-for="product in product.similarProducts" :key="product.id" class="group max-lg:h-[23.1rem] p-5 mr-4 border-2 border-gray">
-            <NuxtLink :to="`/product/${product.id}/${product.slug}`">
+        <UCarousel v-slot="{ item }" :items="product.similarProducts" class="flex w-full pt-1 w-screen lg:w-[70rem] xl:w-[86rem]" :ui="{ item: 'basis-full max-sm:basis-1/3 sm:basis-1/3 md:basis-1/5 lg:basis-1/6' }" :prev-button="{ class: 'color: bg-black hover:bg-zinc-900 active:bg-zinc-800' }" :next-button="{ class: 'color: bg-black hover:bg-zinc-900 active:bg-zinc-800' }" arrows>
+    	  <div :key="item.id" class="flex group max-lg:h-[23rem] max-lg:ml-5 lg:mr-5 border-2 border-gray" draggable="false">
+            <NuxtLink :to="`/product/${item.id}/${item.slug}`" class="p-5">
               <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 h-72 xl:h-80 min-w-[15rem]">
-                <NuxtImg :src="product.imageSrc" :alt="product.imageAlt" loading="lazy" class="h-full w-full object-cover object-center" />
+                <NuxtImg :src="item.imageSrc" :alt="item.imageAlt" loading="lazy" class="h-full w-full object-cover object-center" />
               </div>
               <div class="mt-4 flex justify-between">
                 <div>
                   <h3 class="text-sm text-gray-700">
                     <span aria-hidden="true" class="inset-0" />
-                    {{ product.name }}
+                    {{ item.name }}
                   </h3>
                 </div>
                 <p class="text-sm font-medium text-gray-900">{{ product.price }}</p>
               </div>
         	</NuxtLink>
-      	  </li>
-        </ul>
+      	  </div>
+  		</UCarousel>
       </div>
     </div>
     
