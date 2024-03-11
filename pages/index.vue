3775c5030c6181833b1bf991ffa3313836550045
jonathansaandev <jonathansaandev@gmail.com>
2024-03-11 20:20:08 -0300

    card style

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
 
     <ul v-if="!pending && !error" class="mt-14 max-sm:mx-[1rem] max-md:mx-[3rem] max-lg:mx-[6rem] p-3 md:p-0 grid max-md:grid-cols-2 md:grid-cols-3 gap-7 lg:gap-14 items-start mx-auto">
       <li v-for="product in allProducts" :key="product.id" class="flex group border-2 border-gray max-sm:min-h-[63vw]">
-        <NuxtLink :to="`/product/${product.id}/${product.slug}`" class="p-5">
+        <NuxtLink :to="`/product/${product.id}/${product.slug}`" class="p-5 w-full">
           <div class="rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
             <NuxtImg :src="product.imageSrc" :alt="product.imageAlt" loading="lazy" class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
           </div>
