ef404c6b6f5fd4323a91b81dbfc319c7feeabf82
jonathansaandev <jonathansaandev@gmail.com>
2024-03-14 18:56:38 -0300

    lang and charset on nuxt.config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,11 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      htmlAttrs: {
+        lang: "en"
+      },
       title: "CaliSaan",
+      charset: "utf-8",
       meta: [
         { name: "description", content: "Discover top-quality fitness products to help you reach your health and wellness goals. From cutting-edge workout gear to performance-enhancing supplements, CaliSaan has everything you need for a fit and active lifestyle. Start your journey to a healthier you today!" },
         { name: "og:description", content: "Discover top-quality fitness products to help you reach your health and wellness goals. From cutting-edge workout gear to performance-enhancing supplements, CaliSaan has everything you need for a fit and active lifestyle. Start your journey to a healthier you today!" },
