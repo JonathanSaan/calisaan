a9d3fc8d9ec94dd592cdb458e50855d0134d7e0e
jonathansaandev <jonathansaandev@gmail.com>
2024-03-18 12:24:20 -0300

    Fix filterUtils.ts to correctly update category and price in the router

--- utils/filterUtils.ts
+++ utils/filterUtils.ts
@@ -6,24 +6,24 @@ const useFilter = () => {
   const router = useRouter();
   const selectedCategory = ref(route.query.category || "");
   const selectedPrice = ref(route.query.price || "");
-  
+
   const category = computed({
     get: () => {
-      return route.query.category
+      return route.query.category;
     },
     set: (val) => {
-      router.push({ path: route.path, query: { category: val } })
-    }
-  })
-  
+      router.push({ path: route.path, query: { category: val } });
+    },
+  });
+
   const price = computed({
     get: () => {
-      return route.query.price
+      return route.query.price;
     },
     set: (val) => {
-      router.push({ path: route.path, query: { price: val } })
-    }
-  })
+      router.push({ path: route.path, query: { price: val } });
+    },
+  });
 
   const applyFilters = () => {
     const params = { ...route.query };
@@ -39,7 +39,7 @@ const useFilter = () => {
     } else {
       delete params.price;
     }
-    
+
     category.value = selectedCategory.value;
     price.value = selectedPrice.value;
     router.replace({ path: route.path, query: params });
