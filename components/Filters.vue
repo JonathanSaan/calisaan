7aa2c8e421d30e3398f83966e18f3916acb2d505
jonathansaandev <jonathansaandev@gmail.com>
2024-03-18 12:21:27 -0300

    Refactor Filters.vue component

--- components/Filters.vue
+++ components/Filters.vue
@@ -1,16 +1,32 @@
 <template>
   <div>
     <h3 class="mb-3 text-lg font-medium">Category</h3>
-    <UCheckbox v-for="(label, index) in categoryLabels" :key="index" class="ml-3 mb-2" :ui="{ color: 'text-black', base: 'cursor-pointer' }" :value="label" @change="handleCheckboxChange($event, label, 'category')" :checked="isChecked(label, 'category')">
+    <UCheckbox
+      v-for="(label, index) in categoryLabels"
+      :key="index"
+      class="ml-3 mb-2"
+      :ui="{ color: 'text-black', base: 'cursor-pointer' }"
+      :value="label"
+      @change="handleCheckboxChange($event, label, 'category')"
+      :checked="isChecked(label, 'category')"
+    >
       <template #label>
         <span class="cursor-pointer pl-4 ml-[-1rem]">{{ label }}</span>
       </template>
     </UCheckbox>
   </div>
-      
+
   <div class="flex flex-col border-t-[1px] sm:border-t-2 border-gray mt-5 pt-3">
     <h3 class="text-lg font-medium mb-3">Price:</h3>
-    <UCheckbox v-for="(label, index) in priceLabels" :key="index" class="ml-3 mb-2" :ui="{ color: 'text-black', base: 'cursor-pointer' }" :value="label" @change="handleCheckboxChange($event, label)" :checked="isChecked(label, 'price')">
+    <UCheckbox
+      v-for="(label, index) in priceLabels"
+      :key="index"
+      class="ml-3 mb-2"
+      :ui="{ color: 'text-black', base: 'cursor-pointer' }"
+      :value="label"
+      @change="handleCheckboxChange($event, label)"
+      :checked="isChecked(label)"
+    >
       <template #label>
         <span class="cursor-pointer pl-4 ml-[-1rem]">{{ label }}</span>
       </template>
@@ -28,15 +44,15 @@ const priceLabels = ["Under $30.00", "$30.00 - $70.00", "$70.00 - $100.00"];
 const handleCheckboxChange = (event, value, type) => {
   if (type === "category") {
     return selectedCategory.value = event.target.checked ? value : null;
-  } 
-  selectedPrice.value = event.target.checked ? value : null;
+  }
+  return selectedPrice.value = event.target.checked ? value : null;
 };
 
 const isChecked = (label, type) => {
   if (type === "category") {
     return selectedCategory.value === label;
   }
-  selectedPrice.value === label;
+  return selectedPrice.value === label;
 };
 
 watchEffect(() => {
